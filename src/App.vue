854954bf81b4f2a3446bfb9035e16e42cc3eda10
EC2 Default User <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2021-08-22 22:03:20 +0000

    create assignment portfolio using Vue CLI

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+   <a href="https://meanmodemoda.github.io/msdv-summer-workshop-about-me/" target="_blank" rel="noopener"><img alt="Vue logo" src="./assets/logo.png"></a> 
+    <span></span>
+    <h1>MS Data Visualization Summer Workshop 2021</h1>
+    <Assignments msg="Assignment Portfolio"/>
+  </div>
+</template>
+
+<script>
+import Assignments from './components/Assignments.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Assignments
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  height: 150px;
+}
+</style>
---
+++ src/components/Assignments.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <br>
+    <br>
+    <p>A 6 week training outcome to get started with HTML, CSS and JavaScript. Plot graphs using d3 and p5,render JavaScript templates with Vue.js.</p>
+    <ul>
+      <li><div class="container"><a href="https://meanmodemoda.github.io/msdv-summer-workshop-about-me/" target="_blank" rel="noopener">
+        <img alt="Assignment 1" src="./Assignment_1_Square.jpg"> 
+        <div class="overlay"><div class="text"><h3>Assignment 1</h3>
+          <p>Create a responsive "About Me" page.</p></div>
+        </div></a></div></li>
+        
+      <li><div class="container"><a href="https://meanmodemoda.github.io/msdv-summer-workshop-javascript-functions" target="_blank" rel="noopener"> 
+      <img alt="Assignment 2" src="./Assignment_2_Square.jpg">
+      <div class="overlay"><div class="text"><h3>Assignment 2</h3>
+        <p>Practice JavaScript functions and console log results.</p></div>
+      </div></a></div></li>
+      
+      <li><div class="container"><a href="https://meanmodemoda.github.io/msdv-summer-workshop-javascript-data-rendering/" target="_blank" rel="noopener">   
+       <img alt="Assignment 3" src="./Assignment_3_Square.jpg">
+      <div class="overlay"><div class="text"><h3>Assignment 3</h3>
+        <p>Render data using JavaScript in increments of 5 and add pagination.</p></div>
+      </div></a></div></li>
+
+      <li><div class="container"><a href="https://meanmodemoda.github.io/msdv-summer-workshop-d3-bar-chart/" target="_blank" rel="noopener">  
+      <img alt="Assignment 4.1" src="./Assignment_4_1_Square.jpg">
+      <div class="overlay"><div class="text"><h3>Assignment 4.1</h3>
+        <p>Plot a bar chart using d3.js.</p></div>
+      </div></a></div></li>
+      
+        <li><div class="container"><a href="https://meanmodemoda.github.io/msdv-summer-workshop-vue-render-and-filter-data/" target="_blank" rel="noopener">  
+     <img alt="Assignment 5" src="./Assignment_5_Square.jpg">
+      <div class="overlay"><div class="text"><h3>Assignment 5</h3>
+        <p>Use Vue.js to render data and add a text based filter.</p></div>
+      </div></a></div></li>
+      
+    </ul>
+  </div>
+   
+</template>
+
+<script>
+export default {
+  name: 'Assignments',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+
+p {
+  font-size: 20px;
+}
+
+ul {
+  display: inline-block;
+  list-style-type: none;
+  padding: 0;
+}
+
+
+li {
+  display: inline-block;
+  margin: 20px 20px;
+  /*position: relative;*/
+}
+
+a {
+  color: #42b983;
+}
+
+.container {
+  position: relative;
+  border: 0.1rem solid black;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: white;
+}
+
+img {
+  display: block;
+  width: 400px;
+  height: 100%;
+}
+
+.text {
+  color: black;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  opacity: 0;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+}
+
+img:hover {
+  opacity: 0.3;
+}
+
+li:hover .text {
+  opacity: 1;
+}
+
+.container:hover .overlay {
+  opacity: 1;
+}
+
+
+</style>
